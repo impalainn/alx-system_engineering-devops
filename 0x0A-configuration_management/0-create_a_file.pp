c4362bdc71b8119af5147b1c35245d5a72a1c093
impalainn <ahonavictor@gmail.com>
2023-03-24 20:05:11 -0700

    exec

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
-file { '/tmp/school':
-  ensure => 'file',
+file { '/tmp':
+  ensure => 'directory',
   owner  => 'www-data',
   group  => 'www.data',
+}
+file { '/tmp/school':
+  ensure => 'file',
   mode   => '0644',
 }
