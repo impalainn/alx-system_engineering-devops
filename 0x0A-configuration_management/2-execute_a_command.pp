a011fa33d92c77d0845980cb24b3b1a75974891d
impalainn <ahonavictor@gmail.com>
2023-03-30 01:27:54 -0700

    killmenow

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-exec { 'killmenow':
+#Manifest to execute a command to kill a process named killmenow
+
+exec { 'kill':
   command => 'pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
-  onlyif  => 'pgrep -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
 
